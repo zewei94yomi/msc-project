54b5477452f2531cd3c1590ceb5174e084b50cad
ryanzhou94 <ryanzhouzw@foxmail.com>
2022-05-02 15:36:07 +0100

    Update order.py

--- src/orders/order.py
+++ src/orders/order.py
@@ -1,5 +1,6 @@
 from commons.coordinate import Coordinate
 from commons.auto_str import auto_str
+from commons.enum import Status
 import uuid
 from datetime import datetime
 
@@ -10,14 +11,16 @@ class Order:
     Food delivery orders.
     """
     
-    def __init__(self, start: Coordinate, end: Coordinate, description=""):
+    def __init__(self, start_loc: Coordinate, end_loc: Coordinate, time: datetime, description=""):
         self.uuid = uuid.uuid4()  # Unique ID
-        self.generate_time = datetime.today()  # Generated time
-        self.start = start  # Start location
-        self.end = end  # End location
+        self.start_loc = start_loc  # Start location
+        self.end_loc = end_loc  # End location
+        self.generate_time = time  # Generated time
+        self.status = Status.WAITING
         self.description = description  # Order description
-    
+
 
 if __name__ == '__main__':
-    o = Order(Coordinate(1.1, 2.2), Coordinate(2.2, 4.55))
+    o = Order(Coordinate(1.1, 2.2), Coordinate(2.2, 4.55), datetime.today())
     print(o)
+    print(type(o.generate_time))
